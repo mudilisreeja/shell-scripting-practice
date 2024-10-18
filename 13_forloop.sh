05f0ec982729d52088a142ff4df1af316f3d570b
sreeja <sreeja.mudili@gmail.com>
2024-10-18 16:52:05 +0530

    for loop

---
+++ 13_forloop.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#for loop
+ for i in 1 2 3 4 5 6 7 8 9 10
+ do 
+	 echo "number is $i"
+ done
+ #####################
+ for name in raju shyam mani
+ do
+	 echo " names $name"
+ done
+
+ #########################
+ for j in {1..15}
+ do
+	 echo "numbers are $j"
+ done
+
+	 
+
