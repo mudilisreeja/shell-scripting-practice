70348bbbfaabf803e597b259ae83b1924c3f5405
sreeja <sreeja.mudili@gmail.com>
2024-10-19 13:04:38 +0530

    functions

---
+++ 19_functions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+##########function creation and calling
+function welcomeNote {
+
+	echo "-----------------"
+	echo "welcome"
+	echo "----------------"
+
+}
+
+#####To call our function 
+welcomeNote
+
